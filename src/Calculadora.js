d1c6e3b910122baf78cc422738f4b8aaeada74e4
Rafgel <114894870+Rafgel@users.noreply.github.com>
2023-06-11 23:33:22 -0600

    primer comit

---
+++ src/Calculadora.js
@@ -0,0 +1,15 @@
+import React, { useState } from "react";
+import {create,all} from 'mathjs';
+const math = create(all);
+
+
+import  { View,
+          text,
+          TouchableOpacity,
+          StyleSheet
+        } from "react-native";     
+
+
+
+
+export default Calculadora;
