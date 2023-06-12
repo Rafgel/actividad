a647d3796483ec16bac972737ddd0ef86a4ba37d
Rafgel <114894870+Rafgel@users.noreply.github.com>
2023-06-12 01:22:15 -0600

    ultimo commit ya tengo sueño

--- src/Calculadora.js
+++ src/Calculadora.js
@@ -1,33 +1,133 @@
 import React, { useState } from "react";
-import {create,all} from 'mathjs';
+import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
+import { create, all } from 'mathjs';
+
 const math = create(all);
 
-
-import  { View,
-          text,
-          TouchableOpacity,
-          StyleSheet
-        } from "react-native";     
-
-
 const Calculadora = () => {
-    const [reultado, setResultado] = useState('');
+    const [resultado, setResultado] = useState('');
 
-}
+    const handleButtonPress = (value) => {
+        setResultado(resultado + value);
+    };
 
-const handleButtonPress = (value) => {
-    setResultado(resultado) + value
-}
+    const calculadoraResultado = () => {
+        try {
+            const evaluatedResult = math.evaluate(resultado);
+            setResultado(evaluatedResult.toString());
+        } catch (error) {
+            setResultado('Error');
+        }
+    };
 
-const calculadoraResultado = () => {
-    try {
-        const evaluatedResult = math.evaluate(resultado);
-        setResultado(evaluatedResult.toString);
-    } catch (error) {
-        setResultado('Error');
-    }
-}
+    const limpiarResultado = () => {
+        setResultado('');
+    };
 
+    return (
+        <View style={styles.container}>
+            <View style={styles.resultContainer}>
+                <Text style={styles.resultText}>{resultado}</Text>
+            </View>
+            <View style={styles.buttonContainer}>
+                <View style={styles.row}>
+                    <TouchableOpacity style={styles.button} onPress={() => handleButtonPress('7')}>
+                        <Text style={styles.buttonText}>7</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={styles.button} onPress={() => handleButtonPress('8')}>
+                        <Text style={styles.buttonText}>8</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={styles.button} onPress={() => handleButtonPress('9')}>
+                        <Text style={styles.buttonText}>9</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={styles.button} onPress={() => handleButtonPress('/')}>
+                        <Text style={styles.buttonText}>/</Text>
+                    </TouchableOpacity>
+                </View>
+                <View style={styles.row}>
+                    <TouchableOpacity style={styles.button} onPress={() => handleButtonPress('4')}>
+                        <Text style={styles.buttonText}>4</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={styles.button} onPress={() => handleButtonPress('5')}>
+                        <Text style={styles.buttonText}>5</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={styles.button} onPress={() => handleButtonPress('6')}>
+                        <Text style={styles.buttonText}>6</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={styles.button} onPress={() => handleButtonPress('*')}>
+                        <Text style={styles.buttonText}>*</Text>
+                    </TouchableOpacity>
+                </View>
+                <View style={styles.row}>
+                    <TouchableOpacity style={styles.button} onPress={() => handleButtonPress('1')}>
+                        <Text style={styles.buttonText}>1</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={styles.button} onPress={() => handleButtonPress('2')}>
+                        <Text style={styles.buttonText}>2</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={styles.button} onPress={() => handleButtonPress('3')}>
+                        <Text style={styles.buttonText}>3</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={styles.button} onPress={() => handleButtonPress('-')}>
+                        <Text style={styles.buttonText}>-</Text>
+                    </TouchableOpacity>
+                </View>
+                <View style={styles.row}>
+                    <TouchableOpacity style={styles.button} onPress={() => handleButtonPress('0')}>
+                        <Text style={styles.buttonText}>0</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={styles.button} onPress={() => handleButtonPress('.')}>
+                        <Text style={styles.buttonText}>.</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={styles.button} onPress={calculadoraResultado}>
+                        <Text style={styles.buttonText}>=</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={styles.button} onPress={() => handleButtonPress('+')}>
+                        <Text style={styles.buttonText}>+</Text>
+                    </TouchableOpacity>
+                </View>
+                <TouchableOpacity style={styles.button} onPress={limpiarResultado}>
+                    <Text style={styles.buttonText}>C</Text>
+                </TouchableOpacity>
+            </View>
+        </View>
+    );
+};
 
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    resultContainer: {
+        marginBottom: 20,
+    },
+    resultText: {
+        fontSize: 24,
+        fontWeight: 'bold',
+    },
+    buttonContainer: {
+        width: 300,
+    },
+    row: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        marginBottom: 10,
+    },
+    button: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: 'green',
+        padding: 10,
+        borderRadius: 5,
+        margin: 4
+    },
+    buttonText: {
+        fontSize: 20,
+        fontWeight: 'bold'
+    },
+});
 
 export default Calculadora;
